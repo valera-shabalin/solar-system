5052cde3fb758088cbb851f568f1e9c11155007e
Valera Shabalin <shabalinvalera.ts@gmail.com>
2021-05-20 21:31:15 +0700

    Add stars

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three-orbitcontrols-ts';
 
-
 import '@/styles/style.css';
 
 import t_sun from '@/assets/img/t_sun.jpg';
@@ -16,11 +15,13 @@ function Planet(options) {
         texture = options.texture,
         distance = options.distance || 0,
         radius = options.radius,
+        rotationTime = options.rotationTime || 86344,
         t = 0;
 
     const planetGeometry = new THREE.SphereGeometry(DEFAULT_SIZE * radius, 16, 16);
     const planetMaterial = new THREE.MeshPhongMaterial({
-        map: loaderTexture.load(texture)
+        map: loaderTexture.load(texture),
+        flatShading: true
     });
     const mesh = new THREE.Mesh(planetGeometry, planetMaterial);
     mesh.position.set(DEFAULT_DISTANCE * distance, 0, DEFAULT_DISTANCE * distance);
@@ -33,30 +34,13 @@ function Planet(options) {
     }
 
     function updateRotation() {
-        mesh.rotation.y += Math.PI * 2 / 86344 * 30;
-    }
-
-    function setDistance(value) {
-        distance = value;
-    }
-
-    function getDistance() {
-        return distance;
-    }
-
-    function setName(value) {
-        name = value;
-    }
-
-    function getName() {
-        return name;
+        mesh.rotation.y += Math.PI * 2 / rotationTime * 30;
     }
 
     return {
         mesh,
-        updatePosition, updateRotation,
-        setName, getName,
-        setDistance, getDistance
+        updatePosition,
+        updateRotation,
     }
 }
 
@@ -100,7 +84,8 @@ function addPlanets() {
             name: 'Земля',
             radius: 1,
             texture: t_earth,
-            distance: 1
+            distance: 1,
+            rotationTime: 86344
         })
     );
     scene.add(planets[3].mesh);
@@ -119,6 +104,39 @@ function addPlanets() {
     return true
 }
 
+function addStars() {
+    const starsGeometry = new THREE.BufferGeometry();
+    const starsMaterial = new THREE.ParticleBasicMaterial({
+        color: 0xE6E6FA
+    });
+
+    const positions = [];
+    const colors = [];
+    const sizes = [];
+
+    const color = new THREE.Color();
+
+    for (let i = 0; i < 5000; i ++) {
+        positions.push((Math.random() * 2 - 1 ) * 10000);
+        positions.push((Math.random() * 2 - 1 ) * 10000);
+        positions.push((Math.random() * 2 - 1 ) * 10000);
+
+        color.setHSL(i / 100, 1.0, 0.5);
+
+        colors.push(0, 0, 0);
+
+        sizes.push(20);
+    }
+
+    starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+    starsGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    starsGeometry.setAttribute('size', new THREE.Float32BufferAttribute(sizes, 1).setUsage(THREE.DynamicDrawUsage));
+
+    const stars = new THREE.Points(starsGeometry, starsMaterial);
+
+    scene.add(stars);
+}
+
 function init() {
     const container = document.querySelector('#container');
 
@@ -129,7 +147,7 @@ function init() {
 
     /* Create a scene */
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x001230);
+    scene.background = new THREE.Color(0x000010);
 
     /* Create and add a light */
     pointLight = new THREE.PointLight(0xFFFFFF, 1.5);
@@ -157,6 +175,7 @@ function init() {
     loaderTexture = new THREE.TextureLoader();
 
     addPlanets();
+    addStars();
 
     window.addEventListener('resize', onWindowResize);
 }
